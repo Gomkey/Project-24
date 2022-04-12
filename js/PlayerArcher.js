3ffde3f170bff96ef2b2e04da477d59e23cb8b95
Gomkey <96456598+Gomkey@users.noreply.github.com>
2022-04-11 18:52:38 -0700

    Add files via upload

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -11,22 +11,29 @@ class PlayerArcher {
     this.image = loadImage("./assets/playerArcher.png");
 
     World.add(world, this.body);
-    Matter.Body.setAngle(this.body, -90); 
+
+    Matter.Body.setAngle(this.body, -90);
   }
 
   display() {
     var pos = this.body.position;
     var angle = this.body.angle;
+    
 
     if (keyIsDown(DOWN_ARROW) && angle < -73 ) {
       angle += 1;
       Matter.Body.setAngle(this.body, angle);
     }
 
-    if (keyIsDown(UP_ARROW) && angle > -103) {
-      angle -=1;
-      Matter.Body.setAngle(this.body, angle);
+     if (keyIsDown(UP_ARROW) && angle > -103) {
+       angle -=1;
+       Matter.Body.setAngle(this.body, angle);
     }
+
+    
+
+  
+
 
     push();
     translate(pos.x, pos.y);
@@ -36,3 +43,6 @@ class PlayerArcher {
     pop();
   }
 }
+
+
+
